3d69c6635b3f3e6db7c04b30b14df5c0426e9ed4
luisandre69 <luisandre_69@hotmail.com>
2021-02-17 12:14:14 +0000

    base

---
+++ js/JS5.js
@@ -0,0 +1,60 @@
+// function or block scoped
+
+// var - function scoped
+
+// for (var i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// let - block scoped
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// const - block scoped
+
+// for (const i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
+
+// ----------------------------
+
+// redeclaring
+
+// var can be redeclared
+// var cat = "meow";
+
+// var cat = "psssss";
+
+// let cannot be redeclared
+// let dog = "boww";
+
+// let dog = "voww";
+
+// const cannot be redeclared
+// const lion = "roarr";
+
+// const lion = "roooor";
+
+// ------------------------
+
+// redefining
+
+// var can be redefined
+var dog = "boww";
+dog = "voww";
+
+// let can be redefined
+let cat = "meow";
+cat = "prrr";
+
+// const cannot be redefined
+const lion = "roar";
+lion = "rooor";
